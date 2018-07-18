363af1e25e8cfd5c5ee6e56af425ca68dd2e56cc
dream-state <jordan.curd1@gmail.com>
2018-07-17 22:36:01 -0400

    MMX spritesheet, start anims

--- Assets/Project/Scripts/Characters/MovableCharacter.cs
+++ Assets/Project/Scripts/Characters/MovableCharacter.cs
@@ -1,6 +1,11 @@
 using System;
 using UnityEngine;
 
+public enum FacingDirection {
+  LEFT,
+  RIGHT
+}
+
 [RequireComponent(typeof(BoxCollider2D))]
 public class MovableCharacter : MonoBehaviour, IMovableCharacter {
   [SerializeField] private float maxSpeed = 10f;
@@ -9,6 +14,7 @@ public class MovableCharacter : MonoBehaviour, IMovableCharacter {
 
   private Rigidbody2D rigidBody;
   private BoxCollider2D collider;
+  private Animator animator;
   private Transform groundedPoint;
   private Vector2 groundedSize;
   private float curMoveScalar;
@@ -16,6 +22,7 @@ public class MovableCharacter : MonoBehaviour, IMovableCharacter {
   private bool jumping;
   private float airTime;
   private bool releasedJump;
+  private FacingDirection curFacingDir;
 
   public void Move(float moveScalar) {
     curMoveScalar = moveScalar;
@@ -32,8 +39,10 @@ public class MovableCharacter : MonoBehaviour, IMovableCharacter {
   private void Awake() {
     rigidBody = GetComponent<Rigidbody2D>();
     collider = GetComponent<BoxCollider2D>();
+    animator = GetComponent<Animator>();
     groundedPoint = transform.Find("GroundedPoint");
     groundedSize = new Vector2(collider.size.x * 0.98f, 0);
+    curFacingDir = FacingDirection.RIGHT;
   }
 
   private void FixedUpdate() {
@@ -51,6 +60,16 @@ public class MovableCharacter : MonoBehaviour, IMovableCharacter {
   private void Update() {
     // Handle horizontal movement
     var newVelocity = new Vector2(curMoveScalar * maxSpeed, rigidBody.velocity.y);
+
+    // Set movement in animator
+    animator.SetFloat("Speed", Mathf.Abs(curMoveScalar));
+
+    // Handle which direction character should be facing
+    if (curFacingDir == FacingDirection.LEFT && curMoveScalar > 0.0f) {
+      setFacingDir(FacingDirection.RIGHT);
+    } else if (curFacingDir == FacingDirection.RIGHT && curMoveScalar < 0.0f) {
+      setFacingDir(FacingDirection.LEFT);
+    }
 
     // Immediately kill vertical movement if character wants to stop jumping
     if (!jumping && rigidBody.velocity.y > 0.0f) {
@@ -78,5 +97,19 @@ public class MovableCharacter : MonoBehaviour, IMovableCharacter {
 
   private bool shouldJump() {
     return grounded && jumping && releasedJump;
+  }
+
+  private void setFacingDir(FacingDirection dir) {
+    Vector3 newScale = transform.localScale;
+    switch (dir) {
+      case FacingDirection.RIGHT:
+        newScale.x = 1;
+        break;
+      case FacingDirection.LEFT:
+        newScale.x = -1;
+        break;
+    }
+    curFacingDir = dir;
+    transform.localScale = newScale;
   }
 }

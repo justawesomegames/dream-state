7fa0d88db311cc42e6787b4fce2f9afa8c5f3c70
Jordan Curd <jordan.curd1@gmail.com>
2018-07-22 19:59:46 -0400

    Namespace, add some regions, little clean up

--- Assets/Project/Scripts/Characters/Dreamer/Dreamer.cs
+++ Assets/Project/Scripts/Characters/Dreamer/Dreamer.cs
@@ -2,5 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dreamer : MovableCharacter {
+namespace DreamState {
+  public class Dreamer : MovableCharacter {
+  }
 }
--- Assets/Project/Scripts/Characters/MovableCharacter.cs
+++ Assets/Project/Scripts/Characters/MovableCharacter.cs
@@ -1,153 +1,170 @@
-using System;
 using UnityEngine;
-using Global;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(BoxCollider2D))]
-[RequireComponent(typeof(Animator))]
-public class MovableCharacter : MonoBehaviour {
-  [SerializeField] protected float runSpeed = 10f;
-  [SerializeField] protected LayerMask groundLayer;
-  [SerializeField] protected float jumpForce = 800f;
-  [SerializeField] protected Transform groundedPoint;
-  [SerializeField] protected float dashSpeed = 15f;
-  [SerializeField] protected float maxDashTime = 0.4f;
+namespace DreamState
+{
+  [RequireComponent(typeof(Rigidbody2D))]
+  [RequireComponent(typeof(BoxCollider2D))]
+  [RequireComponent(typeof(Animator))]
+  public abstract class MovableCharacter : MonoBehaviour {
+    #region Visible
+    [Header("Movement configuration")]
+    [SerializeField] protected float runSpeed = 10f;
+    [SerializeField] protected float jumpForce = 800f;
+    [SerializeField] protected float dashSpeed = 15f;
+    [SerializeField] protected float maxDashTime = 0.4f;
 
-  protected Rigidbody2D rigidBody;
-  protected BoxCollider2D collider;
-  protected Animator animator;
-  protected Vector2 groundedSize;
-  protected float curMoveScalar;
-  protected bool jumping;
-  protected bool releasedJump;
-  protected Constants.FacingDirection curFacingDir;
-  protected bool dashing;
-  protected float dashTime;
-  protected float xVelocityInAir;
+    [Header("Collision configuration")]
+    [SerializeField] protected LayerMask groundLayer;
+    [SerializeField] protected Transform groundedPoint;
+    #endregion
 
-  private bool _grounded;
-  protected bool grounded {
-    get { 
-      return _grounded; 
-    } 
-    set {
-      _grounded = value;
-      animator.SetBool("Grounded", value);
-    }
-  }
+    #region Internal
+    protected Rigidbody2D rigidBody;
+    protected BoxCollider2D collider;
+    protected Animator animator;
+    protected Vector2 groundedSize;
+    protected float curMoveScalar;
+    protected bool jumping;
+    protected bool releasedJump;
+    protected Global.Constants.FacingDirection curFacingDir;
+    protected bool dashing;
+    protected float dashTime;
+    protected float xVelocityInAir;
 
-  public virtual void Move(float moveScalar) {
-    curMoveScalar = moveScalar;
-  }
-
-  public virtual void Dash(bool dash) {
-    dashing = dash;
-  }
-
-  public virtual void Jump(bool jump) {
-    jumping = jump;
-  }
-
-  protected virtual void Awake() {
-    rigidBody = GetComponent<Rigidbody2D>();
-    collider = GetComponent<BoxCollider2D>();
-    animator = GetComponent<Animator>();
-    groundedSize = new Vector2(collider.size.x * 0.98f, 0);
-    curFacingDir = Constants.FacingDirection.RIGHT;
-    xVelocityInAir = runSpeed;
-  }
-
-  protected virtual void FixedUpdate() {
-    // Determine if character is grounded
-    var newGrounded = false;
-    var groundedPos = new Vector2(groundedPoint.position.x, groundedPoint.position.y);
-    var colliders = Physics2D.OverlapBoxAll(groundedPos, groundedSize, 0, groundLayer);
-    foreach(var col in colliders) {
-      if (col.gameObject != gameObject) {
-        newGrounded = true;
+    private bool _grounded;
+    protected bool grounded {
+      get { 
+        return _grounded; 
+      } 
+      set {
+        _grounded = value;
+        animator.SetBool("Grounded", value);
       }
     }
-    if (newGrounded != grounded) {
-      grounded = newGrounded;
+    #endregion
+
+    #region Virtual functions
+    protected virtual void OnAwake() {}
+    protected virtual void OnUpdate() {}
+    #endregion
+
+    public void Move(float moveScalar) {
+      curMoveScalar = moveScalar;
     }
-  }
 
-  protected virtual void Update() {
-    // Handle horizontal movement
-    var newVelocity = new Vector2((grounded ? runSpeed : xVelocityInAir) * curMoveScalar, rigidBody.velocity.y);
+    public void Dash(bool dash) {
+      dashing = dash;
+    }
 
-    // Handle ground dashing
-    if (dashing && grounded) {
-      dashTime += Time.deltaTime;
-      if (dashTime < maxDashTime) {
-        // Can continue dashing
-        newVelocity.x = dashSpeed;
-        animator.SetBool("Dashing", true);
-        if (curFacingDir == Constants.FacingDirection.LEFT) {
-          newVelocity.x *= -1;
+    public void Jump(bool jump) {
+      jumping = jump;
+    }
+
+    protected void Awake() {
+      rigidBody = GetComponent<Rigidbody2D>();
+      collider = GetComponent<BoxCollider2D>();
+      animator = GetComponent<Animator>();
+      groundedSize = new Vector2(collider.size.x * 0.98f, 0);
+      curFacingDir = Global.Constants.FacingDirection.RIGHT;
+      xVelocityInAir = runSpeed;
+
+      OnAwake();
+    }
+
+    protected void FixedUpdate() {
+      // Determine if character is grounded
+      var newGrounded = false;
+      var groundedPos = new Vector2(groundedPoint.position.x, groundedPoint.position.y);
+      var colliders = Physics2D.OverlapBoxAll(groundedPos, groundedSize, 0, groundLayer);
+      foreach(var col in colliders) {
+        if (col.gameObject != gameObject) {
+          newGrounded = true;
         }
-      } else {
-        // Trying to dash, but already reached max dash time
+      }
+      if (newGrounded != grounded) {
+        grounded = newGrounded;
+      }
+    }
+
+    protected void Update() {
+      // Handle horizontal movement
+      var newVelocity = new Vector2((grounded ? runSpeed : xVelocityInAir) * curMoveScalar, rigidBody.velocity.y);
+
+      // Handle ground dashing
+      if (dashing && grounded) {
+        dashTime += Time.deltaTime;
+        if (dashTime < maxDashTime) {
+          // Can continue dashing
+          newVelocity.x = dashSpeed;
+          animator.SetBool("Dashing", true);
+          if (curFacingDir == Global.Constants.FacingDirection.LEFT) {
+            newVelocity.x *= -1;
+          }
+        } else {
+          // Trying to dash, but already reached max dash time
+          animator.SetBool("Dashing", false);
+        }
+      } else if(dashTime > 0) {
+        dashTime = 0.0f;
         animator.SetBool("Dashing", false);
       }
-    } else if(dashTime > 0) {
-      dashTime = 0.0f;
-      animator.SetBool("Dashing", false);
+
+      // Set movement in animator
+      animator.SetFloat("Speed", Mathf.Abs(newVelocity.x));
+
+      // Handle which direction character should be facing
+      if (curFacingDir == Global.Constants.FacingDirection.LEFT && curMoveScalar > 0.0f) {
+        setFacingDir(Global.Constants.FacingDirection.RIGHT);
+      } else if (curFacingDir == Global.Constants.FacingDirection.RIGHT && curMoveScalar < 0.0f) {
+        setFacingDir(Global.Constants.FacingDirection.LEFT);
+      }
+
+      // Immediately kill y-velocity if character wants to stop jumping
+      if (!jumping && rigidBody.velocity.y > 0.0f) {
+        newVelocity.y = 0.0f;
+      }
+
+      // Set new velocity
+      rigidBody.velocity = newVelocity;
+
+      if (shouldJump()) {
+        grounded = false;
+        releasedJump = false;
+        rigidBody.AddForce(new Vector2(0.0f, jumpForce));
+
+        // If character is dashing while jumping, character can move at dash speed in air
+        xVelocityInAir = dashing ? dashSpeed : runSpeed;
+      }
+
+      // To avoid bounce, keep track of if character released jump before landing
+      if (!jumping && !releasedJump) {
+        releasedJump = true;
+      }
+
+      OnUpdate();
     }
 
-    // Set movement in animator
-    animator.SetFloat("Speed", Mathf.Abs(newVelocity.x));
-
-    // Handle which direction character should be facing
-    if (curFacingDir == Constants.FacingDirection.LEFT && curMoveScalar > 0.0f) {
-      setFacingDir(Constants.FacingDirection.RIGHT);
-    } else if (curFacingDir == Constants.FacingDirection.RIGHT && curMoveScalar < 0.0f) {
-      setFacingDir(Constants.FacingDirection.LEFT);
+    protected void OnDrawGizmosSelected() {
+      // Draw a gizmo at the grounded position since it doesn't easily render otherwise
+      Gizmos.DrawSphere(groundedPoint.transform.position, 0.1f);
     }
 
-    // Immediately kill y-velocity if character wants to stop jumping
-    if (!jumping && rigidBody.velocity.y > 0.0f) {
-      newVelocity.y = 0.0f;
+    private bool shouldJump() {
+      return grounded && jumping && releasedJump;
     }
 
-    // Set new velocity
-    rigidBody.velocity = newVelocity;
-
-    if (shouldJump()) {
-      grounded = false;
-      releasedJump = false;
-      rigidBody.AddForce(new Vector2(0.0f, jumpForce));
-
-      // If character is dashing while jumping, character can move at dash speed in air
-      xVelocityInAir = dashing ? dashSpeed : runSpeed;
+    private void setFacingDir(Global.Constants.FacingDirection dir) {
+      Vector3 newScale = transform.localScale;
+      switch (dir) {
+        case Global.Constants.FacingDirection.RIGHT:
+          newScale.x = 1;
+          break;
+        case Global.Constants.FacingDirection.LEFT:
+          newScale.x = -1;
+          break;
+      }
+      curFacingDir = dir;
+      transform.localScale = newScale;
     }
-
-     // To avoid bounce, keep track of if character released jump before landing
-    if (!jumping && !releasedJump) {
-      releasedJump = true;
-    }
-  }
-
-  protected virtual void OnDrawGizmosSelected() {
-    // Draw a gizmo at the grounded position since it doesn't easily render otherwise
-    Gizmos.DrawSphere(groundedPoint.transform.position, 0.1f);
-  }
-
-  private bool shouldJump() {
-    return grounded && jumping && releasedJump;
-  }
-
-  private void setFacingDir(Constants.FacingDirection dir) {
-    Vector3 newScale = transform.localScale;
-    switch (dir) {
-      case Constants.FacingDirection.RIGHT:
-        newScale.x = 1;
-        break;
-      case Constants.FacingDirection.LEFT:
-        newScale.x = -1;
-        break;
-    }
-    curFacingDir = dir;
-    transform.localScale = newScale;
   }
 }

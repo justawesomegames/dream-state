553917e5287d7924a4a054da376ab665bc08a174
Jordan Curd <jordan.curd1@gmail.com>
2018-07-26 22:03:46 -0400

    Lots of physics progress, but still WIP

--- Assets/Project/Scripts/2D/BoxRaycastCollider2D.cs
+++ Assets/Project/Scripts/2D/BoxRaycastCollider2D.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEngine;
 
 namespace DreamState {
@@ -8,22 +9,20 @@ namespace DreamState {
   [RequireComponent(typeof(BoxCollider2D))]
   public class BoxRaycastCollider2D : MonoBehaviour {
     #region Public
-    [HideInInspector]
-    public EdgeCollisionInfo Top;
-    [HideInInspector]
-    public EdgeCollisionInfo Bottom;
-    [HideInInspector]
-    public EdgeCollisionInfo Left;
-    [HideInInspector]
-    public EdgeCollisionInfo Right;
     [SerializeField] private LayerMask collisionMask;
     [SerializeField] private float skinWidth = 0.015f;
     [SerializeField] private float distanceBetweenRays = 0.25f;
     [SerializeField] private int horizontalRayCount = 3;
     [SerializeField] private int verticalRayCount = 3;
+    [HideInInspector] public CollisionInfo Collisions { get {
+      // Singleton
+      if (_collisions == null) _collisions = new CollisionInfo(this);
+      return _collisions;
+    } }
     #endregion
 
     #region Internal
+    private CollisionInfo _collisions;
     private BoxCollider2D boxCollider;
     private RaycastOrigins raycastOrigins;
     private float horizontalRaySpacing;
@@ -53,7 +52,6 @@ namespace DreamState {
       // Update the origin of each ray
       Bounds bounds = boxCollider.bounds;
       bounds.Expand(skinWidth * -2);
-      
       raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
       raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
       raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
@@ -65,41 +63,70 @@ namespace DreamState {
     /// </summary>
     /// <param name="moveAmount">Amount to move</param>
     public void UpdateCollisions(Vector2 moveVector) {
+      ClearCollisions();
       UpdateRaycastOrigins();
-      UpdateTopCollision(moveVector);
-      UpdateBottomCollision(moveVector);
+      UpdateVerticalCollisions(moveVector);
+      UpdateHorizontalCollisions(moveVector);
     }
 
-    private void UpdateTopCollision(Vector2 moveVector) {
-      Top.Clear();
-      if (moveVector.y <= 0.0f) return;
-
+    private void UpdateVerticalCollisions(Vector2 moveVector) {
+      float yDir = Mathf.Sign(moveVector.y);
       var rayLength = Mathf.Abs(moveVector.y) + skinWidth;
-      for (int i = 0; i < verticalRayCount; i ++) {
-        Vector2 rayOrigin = raycastOrigins.topLeft;
+      for (int i = 0; i < verticalRayCount; i++) {
+        Vector2 rayOrigin = yDir == -1 ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
         rayOrigin += Vector2.right * (verticalRaySpacing * i + moveVector.x);
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, collisionMask);
-        if (hit) {
-          Top.hits.Add(hit);
-          rayLength = hit.distance;
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * yDir, rayLength, collisionMask);
+
+        Debug.DrawRay(rayOrigin, Vector2.up * yDir, Color.red);
+
+        // Check if there was actually a collision
+        if (!hit) continue;
+
+        // Add collision to the appropriate collider
+        if (yDir == -1) {
+          Collisions.Bottom.hits.Add(hit);
+        } else {
+          Collisions.Top.hits.Add(hit);
         }
+
+        // Reduce ray length to avoid unnecessary collisions
+        rayLength = hit.distance;
       }
     }
 
-    private void UpdateBottomCollision(Vector2 moveVector) {
-      Bottom.Clear();
-      if (moveVector.y >= 0.0f) return;
+    private void UpdateHorizontalCollisions(Vector2 moveVector) {
+      float xDir = Mathf.Sign(moveVector.x);
+      var rayLength = Mathf.Abs(moveVector.x) + skinWidth;
 
-      var rayLength = Mathf.Abs(moveVector.y) + skinWidth;
-      for (int i = 0; i < verticalRayCount; i ++) {
-        Vector2 rayOrigin = raycastOrigins.bottomLeft;
-        rayOrigin += Vector2.right * (verticalRaySpacing * i + moveVector.x);
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, rayLength, collisionMask);
-        if (hit) {
-          Bottom.hits.Add(hit);
-          rayLength = hit.distance;
+      // Don't cast farther than we need to
+      if (Mathf.Abs(moveVector.x) < skinWidth) rayLength = 2 * skinWidth;
+
+      for (int i = 0; i < horizontalRayCount; i++) {
+        Vector2 rayOrigin = xDir == -1 ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+        rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * xDir, rayLength, collisionMask);
+
+        // Check if there was actually a collision
+        if (!hit) continue;
+        if (hit.distance == 0) continue;
+
+        // Add collision to the appropriate collider
+        if (xDir == -1) {
+          Collisions.Left.hits.Add(hit);
+        } else {
+          Collisions.Right.hits.Add(hit);
         }
+
+        // Reduce ray length to avoid unnecessary collisions
+        rayLength = hit.distance;
       }
+    }
+
+    private void ClearCollisions() {
+      Collisions.Top.Clear();
+      Collisions.Bottom.Clear();
+      Collisions.Left.Clear();
+      Collisions.Right.Clear();
     }
 
     public struct RaycastOrigins {
@@ -109,13 +136,32 @@ namespace DreamState {
       public Vector2 bottomRight;
     }
 
-    public struct EdgeCollisionInfo {
-      public bool colliding;
-      public List<RaycastHit2D> hits;
+    public class CollisionInfo {
+      public EdgeCollisionInfo Top;
+      public EdgeCollisionInfo Bottom;
+      public EdgeCollisionInfo Left;
+      public EdgeCollisionInfo Right;
 
-      public float DistanceToCollision() {
-        // TODO
-        return 0.0f;
+      public CollisionInfo(BoxRaycastCollider2D c) {
+        Top = new EdgeCollisionInfo(c);
+        Bottom = new EdgeCollisionInfo(c);
+        Left = new EdgeCollisionInfo(c);
+        Right = new EdgeCollisionInfo(c);
+      }
+    }
+
+    public class EdgeCollisionInfo {
+      public List<RaycastHit2D> hits;
+      private BoxRaycastCollider2D collider;
+      
+      public EdgeCollisionInfo(BoxRaycastCollider2D c) {
+        collider = c;
+        hits = new List<RaycastHit2D>();
+      }
+
+      public float NearestCollision() {
+        if (hits.Count < 1) return 0.0f;
+        return (from h in hits select h.distance).Min() - collider.skinWidth;
       }
 
       public bool IsColliding() {
@@ -123,7 +169,6 @@ namespace DreamState {
       }
 
       public void Clear() {
-        colliding = false;
         hits.Clear();
       }
     }
--- Assets/Project/Scripts/2D/PhysicsObject2D.cs
+++ Assets/Project/Scripts/2D/PhysicsObject2D.cs
@@ -3,30 +3,95 @@ using UnityEngine;
 
 namespace DreamState {
   [RequireComponent(typeof(BoxRaycastCollider2D))]
-  public abstract class PhysicsObject2D : MonoBehaviour {
+  public class PhysicsObject2D : MonoBehaviour {
     #region Public
     [SerializeField] private float gravityScalar = 1.0f;
-    [SerializeField] private float maxFallSpeed = 10f;
-    [SerializeField] private float maxSlopeAngle = 45f;
+    [SerializeField] private float horizontalAcceleration = 0.3f;
+    [SerializeField] private float terminalVelocity = 0.3f;
     #endregion
 
     #region Internal
     private BoxRaycastCollider2D raycastCollider;
+    private Vector2 currentVelocity;
+    private Vector2 targetVelocity;
     #endregion
 
-    #region Abstract functions
-    protected abstract Vector2 Move();
-    #endregion
+    public void Move(Vector2 moveAmount) {
+      targetVelocity = moveAmount;
+    }
+
+    public bool Grounded() {
+      return raycastCollider.Collisions.Bottom.IsColliding();
+    }
+
+    public void AddForce(Vector2 force) {
+      targetVelocity = force;
+    }
+
+    public void AddForceAbsolute(Vector2 force) {
+      currentVelocity = force;
+    }
 
     private void Awake() {
       raycastCollider = GetComponent<BoxRaycastCollider2D>();
     }
 
     private void Update() {
-      // Handle gravity
-      // Handle slopes
-      // Handle moving platforms
-      // Get move from abstract and apply
+      var newVelocity = CalculateNewVelocity();
+      raycastCollider.UpdateCollisions(newVelocity);
+      newVelocity = AdjustedVelocity(newVelocity);
+      currentVelocity = newVelocity;
+      transform.Translate(currentVelocity);
+
+      // TODO: Handle slopes
+      // TODO: Handle moving platforms
+
+      // Reset target velocity
+      targetVelocity = Vector2.zero;
+    }
+
+    /// <summary>
+    /// Calculate velocity based on current state, intended state, and outside forces
+    /// </summary>
+    /// <returns>New velocity</returns>
+    private Vector2 CalculateNewVelocity() {
+      Vector2 newVelocity = currentVelocity;
+
+      // Horizontal velocity
+      if (currentVelocity.x < targetVelocity.x) {
+        newVelocity.x = Mathf.Min(newVelocity.x + horizontalAcceleration, targetVelocity.x);
+      } else if (currentVelocity.x > targetVelocity.x) {
+        newVelocity.x = Mathf.Max(newVelocity.x - horizontalAcceleration, targetVelocity.x);
+      }
+
+      // Apply gravity
+      newVelocity.y += (Physics2D.gravity.y * gravityScalar) * Time.deltaTime;
+
+      // Make sure to clamp velocity
+      newVelocity.y = Mathf.Clamp(newVelocity.y, -terminalVelocity, Mathf.Infinity);
+
+      return newVelocity;
+    }
+
+    /// <summary>
+    /// Check each edge of the collider and adjust velocity accordingly
+    /// </summary>
+    /// <param name="velocity">Intended velocity to move</param>
+    /// <returns>New velocity accounting for collisions</returns>
+    private Vector2 AdjustedVelocity(Vector2 velocity) {
+      if (raycastCollider.Collisions.Top.IsColliding()) {
+        velocity.y = raycastCollider.Collisions.Top.NearestCollision();
+      }
+      if (raycastCollider.Collisions.Bottom.IsColliding()) {
+        velocity.y = -raycastCollider.Collisions.Bottom.NearestCollision();
+      }
+      if (raycastCollider.Collisions.Right.IsColliding()) {
+        velocity.x = raycastCollider.Collisions.Right.NearestCollision();
+      }
+      if (raycastCollider.Collisions.Left.IsColliding()) {
+        velocity.x = -raycastCollider.Collisions.Left.NearestCollision();
+      }
+      return velocity;
     }
   }
 }

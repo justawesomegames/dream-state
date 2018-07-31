7c8fc287029d72779f86b32829b92d96c0ec6587
dream-state <jordan.curd1@gmail.com>
2018-07-30 22:27:12 -0400

    Separate animation from movable character

--- Assets/Project/Scripts/Characters/Dreamer/Dreamer.cs
+++ Assets/Project/Scripts/Characters/Dreamer/Dreamer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace DreamState {
+  [RequireComponent(typeof(DreamerAnimator))]
   public class Dreamer : MovableCharacter {
   }
 }
--- Assets/Project/Scripts/Characters/MovableCharacter.cs
+++ Assets/Project/Scripts/Characters/MovableCharacter.cs
@@ -6,9 +6,10 @@ namespace DreamState {
   [DisallowMultipleComponent]
   [RequireComponent(typeof(PlatformerPhysics2D))]
   [RequireComponent(typeof(Rigidbody2D))]
-  [RequireComponent(typeof(Animator))]
-  [RequireComponent(typeof(SpriteRenderer))]
   public abstract class MovableCharacter : MonoBehaviour {
+    public WallStick WallStick { get { return wallStick; } }
+    public bool Dashing { get { return holdingDash && curDashTime < maxDashTime; } }
+
     [Header("Movement")]
     [SerializeField] protected float runSpeed = 10f;
     [SerializeField] protected float dashSpeed = 15f;
@@ -25,10 +26,7 @@ namespace DreamState {
 
     protected PlatformerPhysics2D physics;
     protected Rigidbody2D rigidBody;
-    protected Animator animator;
-    protected SpriteRenderer spriteRenderer;
 
-    private bool facingRight;
     private float curDashTime;
     private bool holdingDash;
     private float curMoveSpeed;
@@ -38,11 +36,7 @@ namespace DreamState {
     private float curJumpToleranceTime;
 
     public void HorizontalMove(float moveScalar) {
-      var newMove = Vector2.right * curMoveSpeed * moveScalar;
-      physics.Move(newMove);
-      if (newMove.x != 0.0f) {
-        Flip(newMove.x > 0.0);
-      }
+      physics.Move(Vector2.right * curMoveSpeed * moveScalar);
     }
 
     public virtual void OnJumpPress() {
@@ -59,7 +53,10 @@ namespace DreamState {
       if (grounded || canStillJump) {
         curMoveSpeed = (holdingDash && curDashTime < maxDashTime) ? dashSpeed : runSpeed;
         physics.SetVelocity(Vector2.up * jumpForce);
-      } else if (!didDoubleJump && canDoubleJump) {
+      }
+
+      // Handle double jumping
+      if (canDoubleJump && !grounded && !didDoubleJump) {
         didDoubleJump = true;
         physics.SetVelocity(Vector2.up * doubleJumpForce);
       }
@@ -75,24 +72,22 @@ namespace DreamState {
     }
 
     public virtual void OnDashPress() {
-      initDashFacingDir = facingRight;
+      initDashFacingDir = FacingRight();
     }
 
     public virtual void OnDashHold() {
       holdingDash = true;
       curDashTime += Time.deltaTime;
+      var facingRight = FacingRight();
       if (initDashFacingDir == facingRight && curDashTime < maxDashTime && physics.Grounded()) {
-        animator.SetBool("Dashing", true);
         physics.Move((facingRight ? Vector2.right : Vector2.left) * dashSpeed);
       } else {
-        animator.SetBool("Dashing", false);
       }
     }
 
     public virtual void OnDashRelease() {
       holdingDash = false;
       curDashTime = 0.0f;
-      animator.SetBool("Dashing", false);
     }
 
     private void OnGroundedChange(bool grounded) {
@@ -118,26 +113,13 @@ namespace DreamState {
     private void Awake() {
       physics = GetComponent<PlatformerPhysics2D>();
       rigidBody = GetComponent<Rigidbody2D>();
-      animator = GetComponent<Animator>();
-      spriteRenderer = GetComponent<SpriteRenderer>();
-
       physics.RegisterModifier(wallStick);
       physics.Collisions.Bottom.RegisterCallback(OnGroundedChange);
-
       wallStick.OnWallStickChange(OnWallStickChange);
-
       curMoveSpeed = runSpeed;
-      facingRight = true;
     }
 
     private void Update() {
-      animator.SetBool("Grounded", physics.Grounded());
-      animator.SetFloat("xSpeed", Mathf.Abs(physics.TargetVelocity.x));
-      animator.SetFloat("ySpeed", physics.CurrentVelocity.y);
-      animator.SetBool("StickingToWall", wallStick.StickingToWall);
-
-      spriteRenderer.flipY = physics.GravityDirection() == 1;
-
       // Handle jump tolerance
       if (canStillJump) {
         curJumpToleranceTime += Time.deltaTime;
@@ -147,13 +129,8 @@ namespace DreamState {
       }
     }
 
-    private void Flip(bool right) {
-      if (right != facingRight) {
-        var newScale = transform.localScale;
-        newScale.x = right ? 1 : -1;
-        transform.localScale = newScale;
-      }
-      facingRight = right;
+    private bool FacingRight() {
+      return transform.localScale.x == 1;
     }
   }
 }

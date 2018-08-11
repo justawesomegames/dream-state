5b5e4f18f8e3bdbc4fef2bcda509b44a5c2f4cc8
Jordan Curd <jordan.curd1@gmail.com>
2018-08-10 21:37:07 -0400

    Add character health and resource and incorporate into shooting

--- Assets/Project/Scripts/Abilities/RangedAbility.cs
+++ Assets/Project/Scripts/Abilities/RangedAbility.cs
@@ -1,13 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace DreamState {
   [CreateAssetMenu(menuName = "Abilities/RangedAbility")]
   public class RangedAbility : BaseAbility {
     public BaseProjectile Projectile;
 
-    public override void DoAbility(GameObject caster, Transform origin) {
+    public override void DoAbility(GameObject caster, Transform origin, List<string> damagesWhat) {
       var projectile = Instantiate(Projectile, origin.position, origin.rotation);
-      projectile.Initialize(caster, this);
+      projectile.Initialize(caster, this, damagesWhat);
     }
   }
 }

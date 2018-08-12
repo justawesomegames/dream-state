125cfb4244a3a56c22066f189d4be0fdf44139d5
dream-state <41217008+dream-state@users.noreply.github.com>
2018-08-12 13:44:50 -0400

    Input management, implement pausing game

--- Assets/Project/Scripts/Characters/Dreamer/PlayerInputController.cs
+++ Assets/Project/Scripts/Characters/Dreamer/PlayerInputController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace DreamState {
@@ -11,40 +10,33 @@ namespace DreamState {
     private void Awake() {
       player = GetComponent<Dreamer>();
       abilityManager = GetComponent<AbilityManager>();
+      RegisterInputHandlers();
     }
 
-    private void Update() {
-      player.HorizontalMove(Input.GetAxis(Global.Constants.Input.HORIZONTAL_AXIS));
+    private void RegisterInputHandlers() {
+      // Horizontal movement
+      InputManager.Instance.RegisterEvent(InputContexts.Playing, InputAxes.Horizontal, (moveAmt) => {
+        player.HorizontalMove(moveAmt);
+      });
 
-      if (Input.GetButtonDown(Global.Constants.Input.JUMP)) {
-        player.OnJumpPress();
-      }
-      if (Input.GetButton(Global.Constants.Input.JUMP)) {
-        player.OnJumpHold();
-      }
-      if (Input.GetButtonUp(Global.Constants.Input.JUMP)) {
-        player.OnJumpRelease();
-      }
+      // Jumping
+      InputManager.Instance.RegisterEvent(InputContexts.Playing, InputButtons.Jump, InputButtonActions.Down, player.OnJumpPress);
+      InputManager.Instance.RegisterEvent(InputContexts.Playing, InputButtons.Jump, InputButtonActions.Hold, player.OnJumpHold);
+      InputManager.Instance.RegisterEvent(InputContexts.Playing, InputButtons.Jump, InputButtonActions.Up, player.OnJumpRelease);
 
-      if (Input.GetButtonDown(Global.Constants.Input.DASH)) {
-        player.OnDashPress();
-      }
-      if (Input.GetButton(Global.Constants.Input.DASH)) {
-        player.OnDashHold();
-      }
-      if (Input.GetButtonUp(Global.Constants.Input.DASH)) {
-        player.OnDashRelease();
-      }
+      // Dashing
+      InputManager.Instance.RegisterEvent(InputContexts.Playing, InputButtons.Dash, InputButtonActions.Down, player.OnDashPress);
+      InputManager.Instance.RegisterEvent(InputContexts.Playing, InputButtons.Dash, InputButtonActions.Hold, player.OnDashHold);
+      InputManager.Instance.RegisterEvent(InputContexts.Playing, InputButtons.Dash, InputButtonActions.Up, player.OnDashRelease);
 
-      if (Input.GetButtonDown(Global.Constants.Input.ATTACK)) {
-        abilityManager.OnAbilityDown();
-      }
-      if (Input.GetButton(Global.Constants.Input.ATTACK)) {
-        abilityManager.OnAbilityHold();
-      }
-      if (Input.GetButtonUp(Global.Constants.Input.ATTACK)) {
-        abilityManager.OnAbilityUp();
-      }
+      // Attacking
+      InputManager.Instance.RegisterEvent(InputContexts.Playing, InputButtons.Attack, InputButtonActions.Down, abilityManager.OnAbilityDown);
+      InputManager.Instance.RegisterEvent(InputContexts.Playing, InputButtons.Attack, InputButtonActions.Hold, abilityManager.OnAbilityHold);
+      InputManager.Instance.RegisterEvent(InputContexts.Playing, InputButtons.Attack, InputButtonActions.Up, abilityManager.OnAbilityUp);
+
+      // Pausing
+      InputManager.Instance.RegisterEvent(InputContexts.Playing, InputButtons.Pause, InputButtonActions.Down, GameManager.Instance.Pause);
+      InputManager.Instance.RegisterEvent(InputContexts.Paused, InputButtons.Pause, InputButtonActions.Down, GameManager.Instance.Unpause);
     }
   }
 }
